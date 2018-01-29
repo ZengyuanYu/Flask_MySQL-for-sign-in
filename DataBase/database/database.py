beccdc071f01561e11c8944f45bb1b1233026489
ZengyuanYu <1952284157@qq.com>
2018-01-29 09:48:15 +0800

    数据库增删改查

--- DataBase/database/database.py
+++ DataBase/database/database.py
@@ -17,6 +17,30 @@ db.create_all()
 
 @app.route('/')
 def index():
+    #增加
+    # article1 = Article(title='aaa', content='bbb')
+    # db.session.add(article1)
+    # db.session.commit()
+
+    #查找
+    # result = Article.query.filter(Article.title == 'aaa').all()
+    # print(result)
+
+    #修改
+    # #1.查找需要更改的数据
+    # article1 = Article.query.filter(Article.title == 'aaa').first()
+    # #2.修改值
+    # article1.title = 'new title'
+    # #3.做事务提交
+    # db.session.commit()
+
+    #删除数据库
+    # #1.把需要删除的数据查找出来
+    # article1 = Article.query.filter(Article.content == 'bbb').first()
+    # #2.把该数据删除掉
+    # db.session.delete(article1)
+    # #3.做事务提交
+    # db.session.commit()
     return 'index'
 
 
